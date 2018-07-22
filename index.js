e197e3d8a36b98da3a668406c3051630fbd16a3d
amp5208 <arnoldpelka@gmail.com>
2018-07-22 13:39:59 -0700

    added linted page and basic execution

---
+++ index.js
@@ -0,0 +1,38 @@
+var pretty = require('pretty');
+var fs = require('fs');
+
+/**
+ * Cleans and lints html in website.
+ * Useful to ensure at least flat
+ * web pages are validated.
+ */
+function htmlLinter() {
+  var html = '';
+  var webpageFile = 'index.html';
+
+  function getWebData(callback) {
+    html = fs.readFileSync(webpageFile).toString();
+    callback();
+  }
+
+  function setCleanWebData() {
+    if (!html.trim()) {
+      return console.log('ERROR: No data to save');
+    } else {
+      fs.writeFile(webpageFile, html, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+
+      console.log('Successfully linted html');
+    }
+  }
+
+  getWebData(function() {
+    html = pretty(html);
+    setCleanWebData();
+  });
+}
+
+htmlLinter();
